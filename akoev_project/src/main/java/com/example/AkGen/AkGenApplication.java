3870be1ce559c3c124bea0a3b9c3b9937b52ce74
genaAkoev <akoev_2000_gena@mail.ru>
2021-07-13 14:40:50 +0300

    Коммит практической работы 1. Выполнил Акоев Геннадий

---
+++ akoev_project/src/main/java/com/example/AkGen/AkGenApplication.java
@@ -0,0 +1,13 @@
+package com.example.AkGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkGenApplication.class, args);
+	}
+
+}
---
+++ akoev_project/src/test/java/com/example/AkGen/AkGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AkGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
